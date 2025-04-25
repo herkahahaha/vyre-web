b4d5705d7c17e0363cdb74104fbf6ab3c6ca05b0
Herka Fitra <herkahahaha@gmail.com>
2025-04-25 17:39:01 +0700

    add temp

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Vyre Digital Creative Agency';
+export const SITE_DESCRIPTION = 'We are digital creative agency based on indonnsia, tangerang city.';
